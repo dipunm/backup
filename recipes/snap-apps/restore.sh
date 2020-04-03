bcff01a65139ebee7be1ff6bd82b383a8c828343
Dipun Mistry <dmistry@opentable.com>
2020-04-03 23:26:35 +0100

    fix for snap restore

--- recipes/snap-apps/restore.sh
+++ recipes/snap-apps/restore.sh
@@ -1,7 +1,7 @@
-if [ -f $DIR_STORE/snap.list ]; download
+if [ -f $DIR_STORE/snap.list ]; then
     while read line; do sudo snap install $line; done <$DIR_STORE/snap.list
 fi
 
-if [ -f $DIR_STORE/classic.list ]; download
+if [ -f $DIR_STORE/classic.list ]; then
     while read line; do sudo snap install $line --classic; done <$DIR_STORE/classic.list
 fi
