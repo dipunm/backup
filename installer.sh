7e45f9fec6218ff9ecb78f485c19bd25961398eb
Dipun Mistry <dipunm@gmail.com>
2020-04-12 02:45:23 +0100

    print $HOME into rc file to make this more portable

--- installer.sh
+++ installer.sh
@@ -4,7 +4,7 @@
 BACKUP_USR_ROOT="${BACKUP_USR_ROOT:-"$HOME/.backup"}"
 SCRIPT="
 # Backup tool.
-export BACKUP_USR_ROOT=\"$BACKUP_USR_ROOT\"
+export BACKUP_USR_ROOT=\"$(echo "$BACKUP_USR_ROOT" | sed "s%^$HOME%\$HOME%")\"
 "
 
 pause_continue() {
