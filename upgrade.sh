29f5fd6603a129d074292685c365e9e70ace5a53
Dipun Mistry <dmistry@opentable.com>
2020-04-12 22:19:13 +0100

    simplify upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -6,19 +6,3 @@ BACKUP_INSTALL_OVERRIDE="don't install"
 
 download
 copy_files && install_recipes
-
-rm -rf "$BACKUP_USR_ROOT/src"
-cp -r "$SOURCE/src" "$BACKUP_USR_ROOT"
-
-for recipe in "$SOURCE/recipes"/*
-do 
-(
-    name=$(basename "$recipe")
-    if [ -d "$BACKUP_USR_ROOT/recipes/$name" ]; then
-        rm -rf "$BACKUP_USR_ROOT/recipes/$name"
-        cp -r "$recipe" "$BACKUP_USR_ROOT/recipes/$name"
-    else 
-        cp -r "$SOURCE/recipes/$name" "$BACKUP_USR_ROOT/recipes"
-    fi
-) 
-done
