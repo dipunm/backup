054af7f96c59e801a4e02ded3357e8010ff2848f
Dipun Mistry <dmistry@opentable.com>
2020-04-12 21:34:41 +0100

    fixed dotnetcore restorer

--- src/recipes/dotnetcore/restore.sh
+++ src/recipes/dotnetcore/restore.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-for i in ${!dotnet_sources[@]};
+. ./default.conf
+
+for i in "${!dotnet_sources[@]}";
 do
-    src=${dotnet_sources[$i]}$DIR_TMP/lib/packages-microsoft-prod.deb
+    src="${dotnet_sources[$i]}"
     read -d '' -r -a packages <<< "${dotnet_packages[$i]}";
 
-    wget $src -O $DIR_TMP/lib/packages-microsoft-prod.deb
-    sudo dpkg -i $DIR_TMP/lib/packages-microsoft-prod.deb
+    wget "$src" -O "$DIR_TMP/packages-microsoft-prod.deb"
+    sudo dpkg -i "$DIR_TMP/packages-microsoft-prod.deb"
     sudo apt-get update
     sudo apt-get install apt-transport-https -y
     sudo apt-get update
-    sudo apt-get install ${packages[@]} -y
-
-    rm $DIR_TMP/lib/packages-microsoft-prod.deb
+    sudo apt-get install "${packages[@]}" -y
 done
