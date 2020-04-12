b1174b39a3d6c2def6d8e2b6d8360d38aa6da363
Dipun Mistry <dmistry@opentable.com>
2020-04-12 21:34:57 +0100

    ubuntu focal needed an extra step

--- src/recipes/gnome-network-settings/restore.sh
+++ src/recipes/gnome-network-settings/restore.sh
@@ -4,3 +4,13 @@ system_connections="/etc/NetworkManager/system-connections"
 
 sudo rsync -b --suffix=".backup" "$DIR_STORE/system-connections" "$(dirname $system_connections)"
 sudo chown -R root:root "$system_connections"
+
+read -r -p "Restart network manager? [Y/n]: " response
+case "$response" in
+[nN][oO]|[nN])
+    echo "You may need to restart your machine to see changes."
+;;
+*)
+    sudo systemctl restart network-manager
+;;
+esac
