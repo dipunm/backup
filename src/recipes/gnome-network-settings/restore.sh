a7320a387032e4227e997863eb2aa36cec2ad685
Dipun Mistry <dipunm@gmail.com>
2020-04-13 05:30:20 +0100

    recursive flag needed

--- src/recipes/gnome-network-settings/restore.sh
+++ src/recipes/gnome-network-settings/restore.sh
@@ -2,7 +2,7 @@
 
 system_connections="/etc/NetworkManager/system-connections"
 
-sudo rsync -b --suffix=".backup" "$DIR_STORE/system-connections" "$(dirname $system_connections)"
+sudo rsync -rb --suffix=".backup" "$DIR_STORE/system-connections" "$(dirname $system_connections)"
 sudo chown -R root:root "$system_connections"
 
 read -r -p "Restart network manager? [Y/n]: " response
