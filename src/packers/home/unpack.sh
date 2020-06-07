7435186083003c95151c0f1f3669c462be99f09f
Dipun Mistry <dmistry@opentable.com>
2020-06-07 17:34:42 +0100

    added the option to restore config.

--- src/packers/home/unpack.sh
+++ src/packers/home/unpack.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+ask() {
+    read -r -p $1 response
+    case "$response" in
+    [Yy][Ee][Ss]|[Yy])
+        return 0
+    ;;
+    *)
+        return 1
+    ;;
+    esac
+}
+
 rsync -rab "$DIR_STORE/BACKUP_USR_ROOT"/* "$BACKUP_USR_ROOT"
 cp -r "$DIR_STORE/home/." ~
+
+if ask "Restore the backup config? [y/N]: "; then
+    cp "$DIR_STORE/BACKUP_USR_ROOT/configs/main.conf" "$BACKUP_USR_ROOT/configs"
+fi
