2b5e616f40ee8c79ef110ae2753fbbe854a76c61
Dipun Mistry <dmistry@opentable.com>
2020-06-07 01:07:42 +0100

    syntax fix

--- src/packers/home/pack.sh
+++ src/packers/home/pack.sh
@@ -7,7 +7,7 @@ cp "$BACKUP_USR_ROOT/configs/main.conf" "$DIR_STORE/BACKUP_USR_ROOT/configs"
 . "$SRC_CONFIG"
 
 if [ "${#FILES[@]}" -gt "0" ]; then
-    for path in "${FILES[@]}" do
+    for path in "${FILES[@]}"; do
         mkdir -p "$( dirname "$DIR_STORE/home/$path" )";
         cp -r ~/"$path" "$DIR_STORE/home/$path";
     done
