3aba1f67ee71ec36ea6e6cc952ca563ab9383e9b
Dipun Mistry <dmistry@opentable.com>
2020-06-09 20:32:12 +0100

    print list of debs before continuing

--- src/packers/deb-packages/unpack.sh
+++ src/packers/deb-packages/unpack.sh
@@ -4,6 +4,18 @@ which gdebi > /dev/null || ( sudo apt update && sudo apt install gdebi-core )
 
 . "$DIR_STORE/debs.conf"
 
+LOCALDEBS=( $( find "$DIR_STORE" -type f -name "*.deb" ) )
+allDebs=( $( echo "${SOURCES[@]} ${LOCALDEBS[@]}" )  )
+
+echo $'\n'"The following packages will be installed:"
+for deb in "${allDebs[@]}"
+do
+    echo "> $deb"
+done
+
+read -rs -d '' -t 0.1
+read -rsn1 -p "Press any key to continue..."$'\n'
+
 for deb in "${SOURCES[@]}"
 do
     if [[ "$deb" =~ "^https?:\/\/" ]]; then
@@ -12,7 +24,7 @@ do
     fi
 done
 
-for deb in "$DIR_STORE"/*/*.deb
+for deb in "${LOCALDEBS[@]}"
 do
     echo "installing $(basename "$deb")"
     sudo gdebi "$deb"
