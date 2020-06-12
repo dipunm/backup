42bf85478f48e3a17e7688146d1267eb4aa0749a
Dipun Mistry <dmistry@opentable.com>
2020-06-12 18:06:49 +0100

    make the application local

--- installer.sh
+++ installer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 {
 
-BACKUP_USR_ROOT="${BACKUP_USR_ROOT:-"$HOME/.backup"}"
+if [ -z $BACKUP_USR_ROOT ]; then
+    non_standard_install="true"
+    BACKUP_USR_ROOT="$HOME/.backup"
+fi
+
 SCRIPT="
 # Backup tool.
 export BACKUP_USR_ROOT=\"$(echo "$BACKUP_USR_ROOT" | sed "s%^$HOME%\$HOME%")\"
@@ -10,22 +14,6 @@ export BACKUP_USR_ROOT=\"$(echo "$BACKUP_USR_ROOT" | sed "s%^$HOME%\$HOME%")\"
 pause_continue() {
     read -rsn1 -p "Press any key to continue..."$'\n'
 }
-
-RCs=( '' ~/.zshrc ~/.bashrc )
-case "$(basename "$SHELL")" in
-    bash)
-        SHELLNAME="bash"
-        option=2
-    ;;
-    zsh)
-        SHELLNAME="zsh"
-        option=1
-    ;;
-    *)
-        SHELLNAME="unknown"
-        option=0
-    ;;
-esac
 
 copy_files() {
     hash="$(date '+%s')"
@@ -66,64 +54,45 @@ echo "
 #########################################
 
 ==> MyBackup will be installed to '$BACKUP_USR_ROOT'.
-" && pause_continue
+" && pause_continue || exit 0
 
 mkdir -p "$BACKUP_USR_ROOT" && copy_files && echo "
 Files installed successfully.
 "
 
-echo "Awesome! We will need to install some environment variables to your 
-shell profile and install the mbkp command.
-
-==> We detected that your default shell is $SHELLNAME
-" && pause_continue && echo;
-
-read -r -p "Which one of these profile files should we install to?
-0: None
-1: ~/.zshrc
-2: ~/.bashrc
-
-enter a number [$option]: " choice
-
-echo; # empty line
-
-RC="${RCs["${choice:-"$option"}"]}"
-[ -n "$RC" ] && echo -n "Installing to '$RC'. "
-[ -z "$RC" ] && echo -n "Ok, we won't modify any files. "
-
-echo; # empty line
-
-if [ -n "$RC" ] && [ -f "$RC" ]; then
-    if cat "$RC" | grep BACKUP_USR_ROOT >/dev/null; then
-        echo "
-==> A previous installation was found in '$RC'. Mo changes were made."
-    else
-        echo "$SCRIPT" >> "$RC" && echo "==> Installation to '$RC' completed successfully."
-    fi
-elif [ -n "$RC" ]; then
-    echo "Unable to find profile file: '$RC'."
-    RC=''
-fi
-
 echo;
 
-echo "Installing the mbkp binary..."
-if [ ! -L "/usr/local/bin/mbkp" ]; then
-    sudo ln -sf "$BACKUP_USR_ROOT/src/main.sh" "/usr/local/bin/mbkp" && echo "==> Congrats! You should now be able to use the mbkp command."
+
+if [ ! -L ~/.local/bin/mbkp ]; then
+    rm -rf ~/.local/bin/mbkp;
+    mkdir -p ~/.local/bin
+    echo "Installing the mbkp executable in ~/.local/bin"
+    sudo ln -sf "$BACKUP_USR_ROOT/src/main.sh" ~/.local/bin/mbkp && \
+    echo "==> Congrats! You should now be able to use the mbkp command." || exit 1
 else
     echo "
-==> mbkp binary already exists.
+==> mbkp executable already exists.
 "
 fi
 
-[ -z $RC ] && echo " 
+
+if [ "$non_standard_install" = "true" ]; then
+
+echo " 
 ==> IMPORTANT: Please manually add the following to your shell profile file:
 $SCRIPT
 ==> Thankyou
 "
-echo "Please confirm the installation by running: 
-mbkp --version"
-echo;
+
+fi
+
+echo "
+
+Please confirm the installation by running: 
+mbkp --version
+
+You may need to add ~/.local/bin to your PATH variable if it does not work.
+"
 
 }
 
