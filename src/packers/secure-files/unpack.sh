c21a1e6d7a30b4b694db025db16a9054433f4d4d
Dipun Mistry <dmistry@opentable.com>
2020-06-12 15:58:03 +0100

    ask before replacing secure files

--- src/packers/secure-files/unpack.sh
+++ src/packers/secure-files/unpack.sh
@@ -4,5 +4,9 @@ HOME=~
 TMP="$HOME/.tmp$(date '+%s')"
 trap "rm $TMP.tar" EXIT
 
-gpg --decrypt --pinentry-mode loopback --output "$TMP.tar" "$DIR_STORE/secure.tar.gpg"
+echo "Restoring secure files. This will override your original files."
+gpg --decrypt --pinentry-mode loopback --output "$TMP.tar" "$DIR_STORE/secure.tar.gpg" || exit
+echo "The following files will be replaced:"
+tar -tf "$TMP.tar"
+ask.sh "Replace your secure files?" || exit 0
 tar -xf "$TMP.tar" -C "$HOME" .
